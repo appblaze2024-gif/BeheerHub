249e5dd1261dbbd039772413977d48f25a5916f6
Django Stoutenburg <appblaze2024@gmail.com>
2026-01-07 13:01:47 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-221732430-65ae3",
+  "appId": "1:330309266486:web:6a8cf99a4fc093393afc97",
+  "apiKey": "AIzaSyDOGFc0V0Gjewxc4gV5ZW3Pu4bG6nYEAI0",
+  "authDomain": "studio-221732430-65ae3.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "330309266486"
+};
