d0dc373a4a267a5331116ce9efb6f31af7b57626
Django Stoutenburg <appblaze2024@gmail.com>
2026-01-07 11:59:07 +0000

    kijk het is niet passend de voerugien menu zorg dat in de hele app dit w

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -60,7 +60,7 @@ export default function DashboardPage() {
   ];
 
   return (
-    <div className="bg-background dark:bg-gray-900 p-6 flex flex-col flex-1">
+    <div className="flex flex-col flex-1 p-6">
       <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-4">
         {overviewData.map((data) => (
           <StatCard key={data.title} {...data} />
