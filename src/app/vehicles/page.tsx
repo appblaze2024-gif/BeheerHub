8463fabc98977705e62bbe1783a8283c1c793ce3
Django Stoutenburg <appblaze2024@gmail.com>
2026-01-07 12:11:55 +0000

    nog steeds jeetje..

--- src/app/vehicles/page.tsx
+++ src/app/vehicles/page.tsx
@@ -207,7 +207,7 @@ export default function VehiclesPage() {
           </Card>
 
           {/* Bottom Card: Tabs */}
-          <Tabs defaultValue="actions" className="flex flex-col flex-1 min-h-0">
+          <Tabs defaultValue="actions" className="flex-1 flex flex-col min-h-0">
             <TabsList>
               <TabsTrigger value="actions">Acties</TabsTrigger>
               <TabsTrigger value="maintenance">Onderhoud</TabsTrigger>
