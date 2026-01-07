52ab6876a339e2be751276c62faec40ad55b00a8
Django Stoutenburg <appblaze2024@gmail.com>
2026-01-07 11:43:45 +0000

    verwijderd dit stuk de headers helemaal  (_for element header_)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,5 @@
 import type { Metadata } from "next";
 import { Toaster } from "@/components/ui/toaster";
-import {
-  SidebarProvider,
-  Sidebar,
-  SidebarInset,
-} from "@/components/ui/sidebar";
-import { SidebarNav } from "@/components/sidebar-nav";
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -33,19 +27,9 @@ export default function RootLayout({
         />
       </head>
       <body className="font-body antialiased">
-        <SidebarProvider defaultOpen={true}>
-          <Sidebar collapsible="icon" side="left">
-            <SidebarNav />
-          </Sidebar>
-          <SidebarInset className="flex flex-col">
-            <header className="flex h-16 shrink-0 items-center justify-end border-b bg-gray-300 px-6 dark:bg-gray-800"></header>
-            <div className="flex-1 overflow-y-auto">{children}</div>
-          </SidebarInset>
-        </SidebarProvider>
+        <div className="flex-1 overflow-y-auto">{children}</div>
         <Toaster />
       </body>
     </html>
   );
 }
-
-    
