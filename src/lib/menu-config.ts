560970ded7d3f4041d7cdb966db6566ef7e362cd
Django Stoutenburg <appblaze2024@gmail.com>
2026-01-23 13:16:04 +0000

    maak dit een speelplaats icoon (_for element <PlaygroundIcon>_)

--- src/lib/menu-config.ts
+++ src/lib/menu-config.ts
@@ -29,12 +29,17 @@ function PlaygroundIcon(props: SVGProps<SVGSVGElement>) {
     strokeLinejoin: "round",
     ...props
   }, 
-    createElement('path', { d: "M13.2 22H18a2 2 0 0 0 2-2V8", key: 1 }),
-    createElement('path', { d: "M6 22V8", key: 2 }),
-    createElement('path', { d: "M12 22V8", key: 3 }),
-    createElement('path', { d: "m3.5 15.5 8-8", key: 4 }),
-    createElement('path', { d: "M12 3.5 11 2", key: 5 }),
-    createElement('path', { d: "M20.5 10.5 12 2", key: 6 }),
+    createElement('path', { d: "M2 3h2", key: 1 }),
+    createElement('path', { d: "M20 3h2", key: 2 }),
+    createElement('path', { d: "M6 3v1", key: 3 }),
+    createElement('path', { d: "M10 3v1", key: 4 }),
+    createElement('path', { d: "M14 3v1", key: 5 }),
+    createElement('path', { d: "M18 3v1", key: 6 }),
+    createElement('path', { d: "M4 22V6.1a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2.8", key: 7 }),
+    createElement('path', { d: "M12 11.9V22", key: 8 }),
+    createElement('path', { d: "m6 12 6-6", key: 9 }),
+    createElement('path', { d: "M6 18h12", key: 10 }),
+    createElement('path', { d: "M18 10c0-1.1-.9-2-2-2h-2c-1.1 0-2 .9-2 2v2c0 1.1.9 2 2 2h2c1.1 0 2-.9 2-2v-2z", key: 11 })
   );
 }
 
