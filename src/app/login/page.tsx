93310f936cc1ce915a5fcefb7dd7ea43b7747ac9
Django Stoutenburg <appblaze2024@gmail.com>
2026-01-25 11:23:05 +0000

    maak het deze afbeelding : <a href="https://ibb.co/kVxCr56p"><img src="h

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -35,8 +35,8 @@ export default function LoginPage() {
     <div className="flex h-screen w-screen flex-col items-center justify-center bg-gray-100 dark:bg-gray-950">
       <div className="absolute top-8 left-8 z-10">
          <Image
-          src="https://i.ibb.co/fVSLJxcG/Ontwerp-zonder-titel-2.png"
-          alt="Ontwerp-zonder-titel-2"
+          src="https://i.ibb.co/HLV8MBp0/Ontwerp-zonder-titel-3.png"
+          alt="Ontwerp-zonder-titel-3"
           width={360}
           height={90}
           priority
