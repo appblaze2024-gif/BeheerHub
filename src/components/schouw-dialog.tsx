b5a3cfa4f2570843efe7fcebd0bdbd7b54d00f09
Django Stoutenburg <appblaze2024@gmail.com>
2026-01-26 09:58:29 +0000

    Try fixing this error: `Runtime ReferenceError: AlertDialogFooter is not

--- src/components/schouw-dialog.tsx
+++ src/components/schouw-dialog.tsx
@@ -30,6 +30,7 @@ import {
   AlertDialogCancel,
   AlertDialogContent,
   AlertDialogDescription,
+  AlertDialogFooter,
   AlertDialogHeader,
   AlertDialogTitle,
   AlertDialogTrigger,
@@ -584,7 +585,7 @@ export function SchouwDialog({
             </form>
           </Form>
         </div>
-        <DialogFooter className="p-6 pt-4 border-t flex flex-row justify-between w-full">
+        <DialogFooter className="p-6 pt-4 border-t flex flex-row sm:justify-between w-full">
           <div>
             {schouwing && (
                 <AlertDialog>
