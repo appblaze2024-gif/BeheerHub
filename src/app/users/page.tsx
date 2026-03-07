15096fc394557a6e0db02106c8055320a67f1b10
Django Stoutenburg <appblaze2024@gmail.com>
2026-03-07 14:12:10 +0000

    haal dit weg (_for element <PageHeader>_)

--- src/app/users/page.tsx
+++ src/app/users/page.tsx
@@ -1,17 +1,11 @@
-
 'use client';
 
 import * as React from 'react';
 import { UserManagement } from '@/components/user-management';
-import { PageHeader } from '@/components/page-header';
 
 export default function UsersPage() {
   return (
     <div className="flex flex-col flex-1 p-6 min-h-0 bg-slate-50 overflow-auto">
-      <PageHeader 
-        title="App Gebruikers & Rechten" 
-        description="Beheer hier de accounts die toegang hebben tot BeheerHub en configureer hun specifieke module-rechten."
-      />
       <div className="mt-6 max-w-6xl mx-auto w-full">
         <UserManagement />
       </div>
